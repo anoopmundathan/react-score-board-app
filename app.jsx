7b71ef8c4c56df5b3fd29df73bd4108b331113fd
anoopmundathan <anoop.mundathan@gmail.com>
2017-04-14 14:59:38 +0100

    Decompose application component into smaller component

--- app.jsx
+++ app.jsx
@@ -1,118 +1,55 @@
+function Header(props) {
+
+	return(
+		<div className="heading">
+			<h1>{props.title}</h1>
+		</div>
+	);
+	
+}
+
+Header.propTypes = {
+	title: React.PropTypes.string.isRequired
+}
+
+function Player(props) {
+	return(
+			<div className="player">
+				<div className="player-name">
+					<h2>{props.name}</h2>
+				</div>
+				<div className="player-scoreboard">
+					<div className="player-decrement">
+						<h2>-</h2>
+					</div>
+					<div className="player-score">
+						<h2>{props.score}</h2>
+					</div>
+					<div className="player-increment">
+						<h2>+</h2>
+					</div>
+				</div>
+			</div>
+	);
+}
+
+Player.propTypes = {
+	name: React.PropTypes.string.isRequired,
+	score: React.PropTypes.number.isRequired
+}
+
+
 function Application(props) {
 	
 	// Return virtual dom element
 	return(
 		<div className="scoreboard">
-
-			<div className="heading">
-				<h1>{props.title}</h1>
-			</div>
-
+			<Header title={props.title}/>			
 				<div className="players">
-				<div className="player">
-					<div className="player-name">
-						<h2>Anoop</h2>
-					</div>
-					<div className="player-scoreboard">
-						<div className="player-decrement">
-							<h2>-</h2>
-						</div>
-						<div className="player-score">
-							<h2>100</h2>
-						</div>
-						<div className="player-increment">
-							<h2>+</h2>
-						</div>
-					</div>
+					<Player name="Anoop" score={10}></Player>
+					<Player name="Anvita" score={20}></Player>
+					<Player name="Neethu" score={30}></Player>
 				</div>
-
-				<div className="player">
-					<div className="player-name">
-						<h2>Anoop</h2>
-					</div>
-					<div className="player-scoreboard">
-						<div className="player-decrement">
-							<h2>-</h2>
-						</div>
-						<div className="player-score">
-							<h2>100</h2>
-						</div>
-						<div className="player-increment">
-							<h2>+</h2>
-						</div>
-					</div>
-				</div>
-
-				<div className="player">
-					<div className="player-name">
-						<h2>Anoop</h2>
-					</div>
-					<div className="player-scoreboard">
-						<div className="player-decrement">
-							<h2>-</h2>
-						</div>
-						<div className="player-score">
-							<h2>100</h2>
-						</div>
-						<div className="player-increment">
-							<h2>+</h2>
-						</div>
-					</div>
-				</div>
-
-				<div className="player">
-					<div className="player-name">
-						<h2>Anoop</h2>
-					</div>
-					<div className="player-scoreboard">
-						<div className="player-decrement">
-							<h2>-</h2>
-						</div>
-						<div className="player-score">
-							<h2>100</h2>
-						</div>
-						<div className="player-increment">
-							<h2>+</h2>
-						</div>
-					</div>
-				</div>
-
-				<div className="player">
-					<div className="player-name">
-						<h2>Anoop</h2>
-					</div>
-					<div className="player-scoreboard">
-						<div className="player-decrement">
-							<h2>-</h2>
-						</div>
-						<div className="player-score">
-							<h2>100</h2>
-						</div>
-						<div className="player-increment">
-							<h2>+</h2>
-						</div>
-					</div>
-				</div>
-
-				<div className="player">
-					<div className="player-name">
-						<h2>Anoop</h2>
-					</div>
-					<div className="player-scoreboard">
-						<div className="player-decrement">
-							<h2>-</h2>
-						</div>
-						<div className="player-score">
-							<h2>100</h2>
-						</div>
-						<div className="player-increment">
-							<h2>+</h2>
-						</div>
-					</div>
-				</div>
-
-			</div>
-
 			</div>
 	);
 }
