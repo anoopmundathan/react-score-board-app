0706b14b09e6997946d7ec34f9a5d7d42bbd092d
anoopmundathan <anoop.mundathan@gmail.com>
2017-04-14 12:06:27 +0100

    Create application component

--- app.jsx
+++ app.jsx
@@ -1,4 +1,12 @@
-ReactDOM.render(
-	<h1>Hello World</h1>,
-	document.getElementById('container')
-);
+function Application() {
+	
+	// Return virtual dom element
+	return(
+		<div>
+			<h1>Rendered from application componet</h1>
+		</div>
+	);
+
+}
+
+ReactDOM.render(<Application/>, document.getElementById('container'));
