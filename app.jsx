19c57d1ab66da56da5f428e0a5591c76928cc389
anoopmundathan <anoop.mundathan@gmail.com>
2017-04-15 14:27:46 +0100

    Communicating Events

--- app.jsx
+++ app.jsx
@@ -16,6 +16,27 @@ var PLAYERS = [
 	}
 ];
 
+function PlayerScoreBoard(props) {
+	return (
+			<div className="player-scoreboard">
+				<div className="player-decrement">
+					<h2 onClick={function() {props.onChange(-1)}}>-</h2>
+				</div>
+				<div className="player-score">
+					<h2>{props.score}</h2>
+				</div>
+				<div className="player-increment">
+					<h2 onClick={function() {props.onChange(1)}}>+</h2>
+				</div>
+			</div>
+	);
+}
+
+PlayerScoreBoard.PropTypes = {
+	score: React.PropTypes.number.isRequired,
+	onChange: React.PropTypes.func.isRequired
+}
+
 function Header(props) {
 
 	return(
@@ -36,35 +57,20 @@ function Player(props) {
 				<div className="player-name">
 					<h2>{props.name}</h2>
 				</div>
-				<PlayerScoreBoard score={props.score}/>
+				<PlayerScoreBoard 
+					score={props.score}
+					onChange = {props.onScoreChange}
+					/>
 			</div>
 	);
 }
 
 Player.propTypes = {
-	name: React.PropTypes.string.isRequired
+	name: React.PropTypes.string.isRequired,
+	onScoreChange: React.PropTypes.func.isRequired
 }
 
-function PlayerScoreBoard(props) {
-	return (
-			<div className="player-scoreboard">
-				<div className="player-decrement">
-					<h2>-</h2>
-				</div>
-				<div className="player-score">
-					<h2>{props.score}</h2>
-				</div>
-				<div className="player-increment">
-					<h2>+</h2>
-				</div>
-			</div>
-	);
-}
-
-PlayerScoreBoard.PropTypes = {
-	score: React.PropTypes.number.isRequired
-}
-
+// Stateful class component
 var Application = React.createClass({
 	propTypes: {
 		title: React.PropTypes.string,
@@ -84,18 +90,28 @@ var Application = React.createClass({
 				players: this.props.initialPlayers
 		}
 	},
-
+	onScoreChange: function(delta, index) {
+		console.log(delta, index);
+		this.state.players[index].score += delta;
+		this.setState(this.state);
+	},
 	render: function() {
 		return(
 			<div className="scoreboard">
 				<Header title={this.props.title}/>			
 					<div className="players">
-						{this.state.players.map(function(player) {
-							return <Player name={player.name} score={player.score} key={player.id}/>
-						})}
+						{this.state.players.map(function(player, index) {
+							return (
+								<Player 
+									onScoreChange={function(delta) {this.onScoreChange(delta, index)}.bind(this)}
+									name={player.name} 
+									score={player.score} 
+									key={player.id}/>
+							);
+						}.bind(this))}
 					</div>
 			</div>
-		);
+		); // end of return
 	}
 	
 });
