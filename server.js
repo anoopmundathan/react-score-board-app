36c9889873bf28086e5cbf9f479480e96bb942d5
anoopmundathan <anoop.mundathan@gmail.com>
2017-05-08 05:28:13 +0100

    Add express server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log('Server running at port', PORT);
+});
